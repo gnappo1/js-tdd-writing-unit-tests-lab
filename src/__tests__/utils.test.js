0007922e3738c455e0a3ce6ee8d96df61e6149ab
Matteo Piccini <gnappo.1@live.it>
2023-07-28 06:57:10 -0700

    done

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,32 @@
 // Your tests here
+import { isPalindrome } from '../utils';
+
+describe('isPalindrome', () => {
+    it ('the function exists', () => {
+        expect(isPalindrome).toBeDefined();
+    })
+
+    // it('expects one parameter of type string', () => {
+    //     expect(isPalindrome).toBeCalledWith(expect.any(String));
+    // })
+
+    it('returns true if the string provided is palindrome', () => {
+        const word1 = 'racecar';
+        const word2 = 'madam';
+        const word3 = 'kayak';
+        
+        expect(isPalindrome(word1)).toBe(true);
+        expect(isPalindrome(word2)).toBe(true);
+        expect(isPalindrome(word3)).toBe(true);
+    })
+
+    it('returns false if the string provided is not palindrome', () => {
+        const word1 = 'hello';
+        const word2 = 'world';
+        const word3 = 'javascript';
+        
+        expect(isPalindrome(word1)).toBe(false);
+        expect(isPalindrome(word2)).toBe(false);
+        expect(isPalindrome(word3)).toBe(false);
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+/**
+ * @param {string} word The word
+ */
+export const isPalindrome = (word) => {
+    const half = Math.floor(word.length / 2)
+    for (let i = 0; i < half; i++) {
+        if (word[i] !== word[word.length - i - 1]) {
+            return false
+        }
+    }
+    return true
+}
